7a75538e14460743982fec51ea0684f6e82518a6
zainalibhinder <109630795+zainalibhinder@users.noreply.github.com>
2023-07-06 16:42:39 -0400

    Add files via upload

---
+++ Code.ino
@@ -0,0 +1,87 @@
+#include <MQ135.h>
+#include "MQ135.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+
+#define sensor         32                        //pin for analog input
+#define buzzer  12  //pin for buzzer192.168.2.21
+
+
+ 
+const char* ssid = "BELL918";
+const char* password =  "2A326E47FD65";
+const char* mqttServer = "192.168.2.21";
+const int mqttPort = 1883;
+const char* mqttUser = "Dhanish";
+const char* mqttPassword = "12345";
+
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+void setup() {
+
+ pinMode(sensor,INPUT);                     //MQ135 analog feed set for input
+  //pinMode(digTrigger,INPUT);                  //MQ135 digital feed set for input
+  pinMode(buzzer,OUTPUT);                        //led set for output
+
+       
+  
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+  
+}
+ 
+void loop() {
+  client.loop();
+  MQ135 gasSensor = MQ135(sensor);
+    int air_quality = gasSensor.getPPM();
+    Serial.print("Air Quality: ");  
+    Serial.print(air_quality);
+    Serial.println("  PPM");   
+    Serial.println();
+    char mqtt_payload[30] = "";
+  snprintf (mqtt_payload,30, "%1d",  air_quality);
+  client.publish("esp/test", mqtt_payload);
+  if (air_quality>1000){
+     digitalWrite(buzzer,LOW);
+   }
+  else {
+     digitalWrite(buzzer,HIGH);
+    }
+    Serial.println("Waiting...");
+
+    delay(2000);    
+    
+  
+}
+
+
+
+
+  
